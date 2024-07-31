d4cb0ec90432d3b88a65e00c41cad407e43c6806
PokiGames <myrchick323@gmail.com>
2024-07-31 18:26:58 +0300

    0.4.3 Edit Button

--- app/src/main/java/com/example/marshroute/MainActivity.kt
+++ app/src/main/java/com/example/marshroute/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
@@ -16,14 +17,15 @@ class MainActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
         setContentView(R.layout.activity_main)
 
+        // Використовуйте FloatingActionButton для buttonAddPoint
         buttonAddPoint = findViewById(R.id.button_add)
-        buttonEditPoints = findViewById(R.id.button_edit)
-
         buttonAddPoint.setOnClickListener {
             val intent = Intent(this, AddPointActivity::class.java)
             startActivity(intent)
         }
 
+        // Використовуйте Button для buttonEditPoints
+        buttonEditPoints = findViewById(R.id.button_edit)
         buttonEditPoints.setOnClickListener {
             val intent = Intent(this, EditPointsActivity::class.java)
             startActivity(intent)
